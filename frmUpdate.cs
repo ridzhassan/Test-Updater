5341682bdda8b1816942ddf3cb699b4baee0204f
ridz <ridz.hassan30@gmail.com>
2025-12-11 11:10:18 +0800

    initial commit

---
+++ frmUpdate.cs
@@ -0,0 +1,26 @@
+﻿using AutoUpdaterDotNET;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test_Updater
+{
+    public partial class frmUpdate : Form
+    {
+        public frmUpdate()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCheckforUpdates_Click(object sender, EventArgs e)
+        {
+            AutoUpdater.Start("https://yourserver.com/update.xml");
+        }
+    }
+}
